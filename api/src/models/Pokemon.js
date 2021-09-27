43ab967642bfbd0e627daea03ff29f6def18a2e5
Estigmaak <ignaestiga@gmail.com>
2021-09-27 19:52:54 -0300

    Models

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -4,9 +4,54 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pokemon', {
-    name: {
+    id: { // ID UUIDV4
+      type: DataTypes.UUID,
+      allowNull: false,
+      primaryKey: true,
+    },
+
+    name: { // NAME POKE
       type: DataTypes.STRING,
       allowNull: false,
     },
+
+    hp: { // PTS VIDA
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+
+    attack: { //PTS ATAQUE/FUERZA
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+
+    defense: { // PTS DEFENSA
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    },
+
+    speed: { // PTS VELOCIDAD
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    height: { // ALTURA
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    
+    weight: { // PESO
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+
+    image: { //IMAGEN
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  
+  {
+    timestamps: false,
   });
 };
